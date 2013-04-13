0e40149a64321a7d5439b602d23be77a2f835d49
David Szotten <davidszotten@gmail.com>
2013-04-13 15:11:41 +0100

    more tests

--- tests/scripts/test_config.py
+++ tests/scripts/test_config.py
@@ -1,9 +1,11 @@
+import argparse
 import pytest
-from mock import patch, call
+from mock import patch, call, sentinel, Mock
 
-from gonzo.scripts.config import set_cloud
+from gonzo.scripts.config import (set_cloud, available_regions, set_region,
+    set_project, print_config, main, init_parser)
 from gonzo.exceptions import ConfigurationError
-from gonzo.test_utils import assert_has_calls
+from gonzo.test_utils import assert_has_calls, assert_called_once_with
 
 
 @patch('gonzo.scripts.config.global_state')
@@ -62,5 +64,72 @@ class TestSetCloud(object):
             set_cloud('foo')
 
 
+@patch('gonzo.scripts.config.config_proxy')
 class TestAvailableRegions(object):
-    pass
+    def test_ok(self, config_proxy):
+        config_proxy.CLOUD = {
+            'REGIONS': sentinel.regions,
+        }
+        assert available_regions() == sentinel.regions
+
+    def test_missing(self, config_proxy):
+        config_proxy.CLOUD = {}
+        assert available_regions() == None
+
+
+@patch('gonzo.scripts.config.global_state')
+class TestSetRegion(object):
+    def test_noop(self, global_state):
+        set_region(None)
+        assert global_state.__setitem__.call_count == 0
+
+    def test_set(self, global_state):
+        set_region('foo')
+        assert_called_once_with(global_state.__setitem__, 'region', 'foo')
+
+
+@patch('gonzo.scripts.config.local_state')
+class TestSetProject(object):
+    def test_noop(self, local_state):
+        set_project(None)
+        assert local_state.__setitem__.call_count == 0
+
+    def test_set(self, local_state):
+        set_project('foo')
+        assert_called_once_with(local_state.__setitem__, 'project', 'foo')
+
+@patch('gonzo.scripts.config.print', create=True)
+@patch('gonzo.scripts.config.global_state')
+@patch('gonzo.scripts.config.local_state')
+def test_print_config(local_state, global_state, print_):
+    local_state.get.return_value = 'bar'
+    data = ['foo', None]
+    global_state.get = lambda x: data.pop(0)
+    print_config()
+    calls = [
+        call('cloud: foo'),
+        call('region: None'),
+        call('project: bar'),
+    ]
+    assert_has_calls(print_, calls)
+
+
+@patch('gonzo.scripts.config.set_cloud')
+@patch('gonzo.scripts.config.set_region')
+@patch('gonzo.scripts.config.set_project')
+class TestMain():
+    def test_ok(self, project, region, cloud):
+        args = Mock()
+        main(args)
+
+    def test_error(self, project, region, cloud):
+        project.side_effect = ConfigurationError()
+        args = Mock()
+        main(args)
+
+
+def test_parser():
+    # just make sure it parses ok
+    parser = argparse.ArgumentParser()
+    init_parser(parser)
+
