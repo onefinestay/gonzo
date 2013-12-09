c67d2ee024451705972183081abcd902ce2b9977
Tom Maddox <tom.maddox@onefinestay.com>
2013-12-09 16:31:01 +0000

    Refactored BotoCfn out to a seperate extension of the base stack class. This allows all the botocfn to be in one place but be used by openstack and aws

--- gonzo/backends/aws/cloud.py
+++ gonzo/backends/aws/cloud.py
@@ -18,6 +18,16 @@ class Cloud(BaseCloud):
             instances += reservation.instances
         return map(self.instance_class, instances)
 
+    def _list_stacks(self):
+        stacks = self.orchestration_connection.list_stacks()
+        return map(self.stack_class, [s.stack_id for s in stacks])
+
+    def get_stack(self, stack_name_or_id):
+        potential_stacks = self.orchestration_connection.describe_stacks(
+            stack_name_or_id=stack_name_or_id)
+
+        return self.stack_class(potential_stacks[0].stack_id)
+
     def list_security_groups(self):
         return self.compute_connection.get_all_security_groups()
 
@@ -143,3 +153,8 @@ class Cloud(BaseCloud):
         )
 
         return self.stack_class(stack_id)
+
+    def terminate_stack(self, stack_name_or_id):
+        stack = self.stack_class(stack_name_or_id)
+        stack.delete()
+        return stack
--- gonzo/backends/base/stack.py
+++ gonzo/backends/base/stack.py
@@ -1,5 +1,9 @@
 from abc import abstractproperty, abstractmethod
+from boto.exception import BotoServerError
+
 from gonzo.backends import get_current_cloud
+from gonzo.config import config_proxy as config
+from gonzo.exceptions import NoSuchResourceError
 
 
 class BaseStack(object):
@@ -61,16 +65,55 @@ class BaseStack(object):
         pass
 
 
-class BaseStackResource(object):
+class BotoCfnStack(BaseStack):
 
-    running_state = abstractproperty
+    running_state = 'CREATE_COMPLETE'
 
-    def __init__(self, resource_id):
-        self._res_id = resource_id
-        self._refresh()
+    @property
+    def description(self):
+        return self._parent.description
 
-    def _refresh(self):
+    @property
+    def status(self):
+        return self._parent.stack_status
+
+    @property
+    def region(self):
+        return config.REGION
+
+    @property
+    def launch_time(self):
+        return self._parent.creation_time
+
+    @property
+    def name(self):
+        return self._parent.stack_name
+
+    @property
+    def resources(self):
         orchestration_connection = get_current_cloud().orchestration_connection
-        self._parent = orchestration_connection.describe_stacks(
-            stack_name_or_id=self.id
-        )[0]
+        return orchestration_connection.describe_stack_resources(
+            stack_name_or_id=self.name,
+        )
+
+    @property
+    def outputs(self):
+        return self._parent.outputs
+
+    @property
+    def is_complete(self):
+        try:
+            self._refresh()
+            return self._parent.stack_status in (
+                'CREATE_FAILED', 'CREATE_COMPLETE', 'ROLLBACK_FAILED',
+                'ROLLBACK_COMPLETE', 'DELETE_FAILED', 'DELETE_COMPLETE',
+                'UPDATE_COMPLETE', 'UPDATE_ROLLBACK_FAILED',
+                'UPDATE_ROLLBACK_COMPLETE')
+        except BotoServerError as err:
+            if err.error_message.__contains__('could not be found'):
+                raise NoSuchResourceError
+            else:
+                raise err
+
+    def delete(self):
+        self._parent.delete()
