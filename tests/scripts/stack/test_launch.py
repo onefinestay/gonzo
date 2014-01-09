f2f802b4ba3da996d410d0c2d1db939f80ce27f9
Tom Maddox <tom.maddox@onefinestay.com>
2014-01-09 17:12:59 +0000

    Revised launch/terminate tests for new creating/removing dns functionality

--- tests/scripts/stack/test_launch.py
+++ tests/scripts/stack/test_launch.py
@@ -7,10 +7,16 @@ from gonzo.scripts.stack.launch import launch
 @patch('gonzo.scripts.stack.launch.wait_for_stack_complete')
 @patch('gonzo.scripts.stack.launch.launch_stack')
 def test_launch(launch_stack, wait_for_stack_complete, print_stack):
-    launch_stack.return_value = Mock(name='stackname')
-    params = Mock(stack_name='stackname', template='template')
+    instance = Mock(name="instance")
+    stack = Mock(name='stack')
+    stack.get_instances.return_value = [instance]
+
+    launch_stack.return_value = stack
+
+    params = Mock(stack_name='stack', template='template')
     launch(params)
 
     assert launch_stack.called
+    assert instance.create_dns_entries_from_tag.called
     assert wait_for_stack_complete.called
     assert print_stack.called
