ae3eaeffb604eba1c83e55b53cb080dcdfce070e
Tom Maddox <tom.maddox@onefinestay.com>
2014-01-10 14:16:02 +0000

    Refactored get_data to a document loader module

--- gonzo/backends/__init__.py
+++ gonzo/backends/__init__.py
@@ -1,12 +1,6 @@
-import os
-from urlparse import urlparse
-
-from jinja2 import Environment
-import requests
-
 from gonzo.aws.route53 import Route53
 from gonzo.config import config_proxy as config
-from gonzo.exceptions import DataError
+from gonzo.helpers.document_loader import get_parsed_document
 
 
 def get_current_cloud():
@@ -54,7 +48,7 @@ def launch_instance(env_type, size=None,
                 the defaults when generating user-data.
             security_groups (list): List of security groups to create (if
                 necessary) and supplement the defaults.
-            username (string): username to set as owner
+            owner (string): username to set as owner
     """
 
     cloud = get_current_cloud()
@@ -86,12 +80,10 @@ def launch_instance(env_type, size=None,
         create_if_not_exist_security_group(security_group)
 
     user_data = None
-    user_data_uri_config_key = 'DEFAULT_USER_DATA'
-    user_data_params_config_key = 'USER_DATA_PARAMS'
-    user_data_uri = config.get_cloud_config(user_data_uri_config_key,
+    user_data_uri = config.get_cloud_config('DEFAULT_USER_DATA',
                                             override=user_data_uri)
     if user_data_uri is not None:
-        user_data = get_data(name, user_data_uri, user_data_params_config_key,
+        user_data = get_parsed_document(name, user_data_uri, 'USER_DATA_PARAMS',
                              user_data_params)
 
     return cloud.launch(
@@ -103,12 +95,10 @@ def launch_stack(stack_name, template_uri, template_params):
     """ Launch stacks """
     stack_name = get_next_hostname(stack_name)
 
-    template_uri_config_key = 'DEFAULT_ORCHESTRATION_TEMPLATE'
-    template_params_config_key = 'ORCHESTRATION_TEMPLATE_PARAMS'
-    template_uri = config.get_cloud_config(template_uri_config_key,
+    template_uri = config.get_cloud_config('DEFAULT_ORCHESTRATION_TEMPLATE',
                                            override=template_uri)
-    template = get_data(stack_name, template_uri,
-                        template_params_config_key, template_params)
+    template = get_parsed_document(stack_name, template_uri,
+                        'ORCHESTRATION_TEMPLATE_PARAMS', template_params)
 
     cloud = get_current_cloud()
     return cloud.launch_stack(stack_name, template)
@@ -126,84 +116,6 @@ def add_default_security_groups(server_type, additional_security_groups=None):
     security_groups = list(set(security_groups))
 
     return security_groups
-
-
-def get_data(entity_name, uri=None, config_params_key=None,
-             additional_params=None):
-    """ Fetch a document from uri specified by `uri` and parse as a template.
-    Template parameters include cli, config and predefined dictionaries.
-    Useful for building CloudFormation templates or UserData scripts.
-    """
-    user_data_params = build_params_dict(entity_name, config_params_key,
-                                         additional_params)
-    return load_data(uri, user_data_params)
-
-
-def build_params_dict(entity_name, config_params_key, additional_params=None):
-    """ Returns a dictionary of parameters to use when rendering CloudFormation
-    templates or user data scripts from template.
-
-    Parameter sources include gonzo defined defaults, cloud configuration and
-    a comma separated key value command line argument. They get overridden in
-    that order. """
-    params = {
-        'hostname': entity_name,
-        'stackname': entity_name,
-        'domain': config.CLOUD['DNS_ZONE'],
-        'fqdn': "%s.%s" % (entity_name, config.CLOUD['DNS_ZONE']),
-    }
-
-    if config_params_key in config.CLOUD:
-        params.update(config.CLOUD[config_params_key])
-
-    if additional_params is not None:
-        params.update(additional_params)
-
-    return params
-
-
-def load_data(uri, params=None):
-    """ Attempt to fetch user data from URL or file. And render, replacing
-     parameters """
-
-    if uri is None:
-        raise ValueError("Document URI cannot be None")
-
-    try:
-        urlparse(uri)
-        data = fetch_from_url(uri)
-    except requests.exceptions.MissingSchema:
-        # Not a url. possibly a file.
-        uri = os.path.expanduser(uri)
-        uri = os.path.abspath(uri)
-
-        if os.path.isabs(uri):
-            try:
-                data = open(uri, 'r').read()
-            except IOError as err:
-                err_msg = "Failed to read from file: {}".format(err)
-                raise DataError(err_msg)
-        else:
-            # Not url nor file.
-            err_msg = "Unknown UserData source: {}".format(uri)
-            raise DataError(err_msg)
-    except requests.exceptions.ConnectionError as err:
-        err_msg = "Failed to read from URL: {}".format(err)
-        raise DataError(err_msg)
-
-    if not params:
-        return data
-
-    data_tpl = Environment().from_string(data)
-    return data_tpl.render(params)
-
-
-def fetch_from_url(url):
-    resp = requests.get(url)
-    if resp.status_code != requests.codes.ok:
-        raise requests.exceptions.ConnectionError("Bad response")
-
-    return resp.text
 
 
 def configure_instance(instance):
---
+++ gonzo/helpers/document_loader.py
@@ -0,0 +1,86 @@
+import os
+from urlparse import urlparse
+
+from jinja2 import Environment
+import requests
+
+from gonzo.config import config_proxy as config
+from gonzo.exceptions import DataError
+
+
+def get_parsed_document(entity_name, uri=None, config_params_key=None,
+             additional_params=None):
+    """ Fetch a document from uri specified by `uri` and parse as a template.
+    Template parameters include cli, config and predefined dictionaries.
+    Useful for building CloudFormation templates or UserData scripts.
+    """
+    user_data_params = build_params_dict(entity_name, config_params_key,
+                                         additional_params)
+    return get_document(uri, user_data_params)
+
+
+def build_params_dict(entity_name, config_params_key, additional_params=None):
+    """ Returns a dictionary of parameters to use when rendering CloudFormation
+    templates or user data scripts from template.
+
+    Parameter sources include gonzo defined defaults, cloud configuration and
+    a comma separated key value command line argument. They get overridden in
+    that order. """
+    params = {
+        'hostname': entity_name,
+        'stackname': entity_name,
+        'domain': config.CLOUD['DNS_ZONE'],
+        'fqdn': "%s.%s" % (entity_name, config.CLOUD['DNS_ZONE']),
+    }
+
+    if config_params_key in config.CLOUD:
+        params.update(config.CLOUD[config_params_key])
+
+    if additional_params is not None:
+        params.update(additional_params)
+
+    return params
+
+
+def get_document(uri, params=None):
+    """ Attempt to fetch user data from URL or file. And render, replacing
+     parameters """
+
+    if uri is None:
+        raise ValueError("Document URI cannot be None")
+
+    try:
+        urlparse(uri)
+        data = fetch_from_url(uri)
+    except requests.exceptions.MissingSchema:
+        # Not a url. possibly a file.
+        uri = os.path.expanduser(uri)
+        uri = os.path.abspath(uri)
+
+        if os.path.isabs(uri):
+            try:
+                data = open(uri, 'r').read()
+            except IOError as err:
+                err_msg = "Failed to read from file: {}".format(err)
+                raise DataError(err_msg)
+        else:
+            # Not url nor file.
+            err_msg = "Unknown UserData source: {}".format(uri)
+            raise DataError(err_msg)
+    except requests.exceptions.ConnectionError as err:
+        err_msg = "Failed to read from URL: {}".format(err)
+        raise DataError(err_msg)
+
+    if not params:
+        return data
+
+    data_tpl = Environment().from_string(data)
+    return data_tpl.render(params)
+
+
+def fetch_from_url(url):
+    resp = requests.get(url)
+    if resp.status_code != requests.codes.ok:
+        raise requests.exceptions.ConnectionError("Bad response")
+
+    return resp.text
