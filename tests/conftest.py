73fbe41592e939a49894bd538f021eed30349697
Tom Maddox <tom.maddox@onefinestay.com>
2014-01-14 15:35:45 +0000

    Switched from a temporary file to dictionary in config fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,5 @@
-from ConfigParser import ConfigParser
-from tempfile import NamedTemporaryFile
 from mock import patch, Mock
 import pytest
-
-from gonzo.config import GlobalState
 
 
 @pytest.yield_fixture
@@ -21,15 +17,8 @@ def minimum_config_fixture():
     with patch('gonzo.config.get_config_module') as get_config_module:
             get_config_module.return_value = Mock(SIZES=sizes,
                                                   CLOUDS={'cloudname': cloud})
-            with NamedTemporaryFile() as tmp_file:
-                a = ConfigParser()
-                a.add_section('gonzo')
-                a.set('gonzo', 'cloud', 'cloudname')
-                a.set('gonzo', 'region', 'regionname')
-                a.write(tmp_file)
-                tmp_file.flush()
-
-                with patch('gonzo.config.global_state',
-                           GlobalState(tmp_file.name)):
-
+            with patch('gonzo.config.global_state', {
+                'cloud': 'cloudname',
+                'region': 'regionname',
+            }):
                     yield get_config_module
