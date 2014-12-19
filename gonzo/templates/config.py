b2a632a86f0e6584bb26b9a29e9bff84df82870b
David Szotten <davidszotten@gmail.com>
2014-12-19 10:23:41 +0000

    flakes

--- gonzo/templates/config.py
+++ gonzo/templates/config.py
@@ -6,10 +6,10 @@ use, and matching authentication details.
 
 CLOUDS = {
     'default': {
-        ### For Openstack based clouds
+        # ### For Openstack based clouds
         'BACKEND': 'gonzo.backends.openstack',
-        ### For AWS based clouds
-        #'BACKEND': 'gonzo.backends.aws',
+        # ### For AWS based clouds
+        # 'BACKEND': 'gonzo.backends.aws',
 
         # Openstack authentication details
         'TENANT_NAME': 'service',
@@ -24,7 +24,7 @@ CLOUDS = {
         'AWS_ACCESS_KEY_ID': '',
         'AWS_SECRET_ACCESS_KEY': '',
 
-        ### Common to either clouds backend
+        # ### Common to either clouds backend
         # Glance or AMI image name
         'IMAGE_NAME': 'Ubuntu 12.04 cloudimg amd64',
 
