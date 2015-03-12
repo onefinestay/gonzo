f2008d06ff80e7336d13ceadf4cb7490b7567abc
Amit <amit@onefinestay.com>
2015-03-12 17:13:42 +0000

    removing deprecated tests

--- tests/scripts/instance/test_security_groups.py
+++
@@ -1,9 +0,0 @@
-from gonzo.backends import add_default_security_groups
-
-
-def test_default_security_groups():
-    groups = add_default_security_groups('server-type', ['gonzo', 'test'])
-
-    # Test for default groups and no duplicates
-    assert groups.count('gonzo') == 1
-    assert sorted(groups) == ['gonzo', 'server-type', 'test']
