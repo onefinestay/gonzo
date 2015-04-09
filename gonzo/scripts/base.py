e422560e2f9c69ec48362f7004a006b6f87b7cfd
Amit <amit@onefinestay.com>
2015-04-09 14:00:10 +0100

    bumping log level

--- gonzo/scripts/base.py
+++ gonzo/scripts/base.py
@@ -17,7 +17,7 @@ def get_parser():
         version='%(prog)s {}'.format(gonzo.VERSION))
     parser.add_argument(
         '--log-level', action='store', help="Log level",
-        default=logging.INFO)
+        default=logging.WARN)
 
     subparsers = parser.add_subparsers(help='subcommand help')
 
