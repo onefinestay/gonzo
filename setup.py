b5ef3dbaa62344e640bf4c5e02456761101ca65b
Amit <amit@onefinestay.com>
2015-07-22 17:44:03 +0100

    update version and change log

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ test_requirements, dependency_links = parse_requirments(
 setup(
     name='gonzo',
     packages=find_packages(exclude=['tests', 'tests.*']),
-    version='0.4.1',
+    version='0.4.2',
     author='onefinestay',
     author_email='engineering@onefinestay.com',
     url='https://github.com/onefinestay/gonzo',
