dcfee7824596eed6901779d54d4afd888aa61ef6
Amit <amit@onefinestay.com>
2015-07-23 15:41:46 +0100

    get region from config is cloud is specified

--- gonzo/clouds/__init__.py
+++ gonzo/clouds/__init__.py
@@ -4,5 +4,9 @@ from gonzo.config import config_proxy
 
 def get_current_cloud(cloud=None):
     cloud_config = config_proxy.get_cloud(cloud)
-    region = config_proxy.REGION
+    if cloud is not None:
+        region = cloud_config['REGIONS'][0]
+    else:
+        region = config_proxy.REGION
+
     return Cloud.from_config(cloud_config, region)
